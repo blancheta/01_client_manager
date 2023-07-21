88c992a52db6a6c59fdbd27b2d999de9f62fc3ec
Daniela Bojica <bojicadaniela@gmail.com>
2023-07-21 12:35:15 +0300

    Exception for amount and intro added

--- client_manager.py
+++ client_manager.py
@@ -1,26 +1,32 @@
 def view_clients(clients):
     if clients:
-        print("\nClient List:")
+        print("Client List:")
         for client in clients:
-            print("\nID:", client['ID'])
+            print("ID:", client['ID'])
             print("Name:", client['Name'])
             print("Address:", client['Address'])
             print("Total Amount Spent:", client['Total Amount Spent'])
+            print()
         print(clients)
     else:
         print("No clients found.")
-
+    print()
 
 def add_client(clients):
     client = {}
     client['ID'] = input("Enter client ID: ")
     client['Name'] = input("Enter client name: ").title()
     client['Address'] = input("Enter client address: ").title()
-    client['Total Amount Spent'] = int(input("Enter total amount spent: "))
-
+    while True:
+        try:
+            client['Total Amount Spent'] = int(input("Enter total amount spent: "))
+        except ValueError:
+            print("Invalid option. Please enter a number.")
+        else:
+            break
     clients.append(client)
     print()
-    print("Client added successfully!")
+    print("Client added successfully!\n")
 
 
 def find_client_by_id(clients, client_id):
@@ -71,20 +77,26 @@ def delete_client(clients):
         print("Client deleted successfully!")
     else:
         print("Client not found.")
+    print()
 
 
 def main():
     clients = []
 
     while True:
-        print("\nClient Management System Menu:")
-        print("1. View My Clients")
-        print("2. Add a Client")
-        print("3. Edit a Client")
-        print("4. Delete a Client")
-        print("5. Return to Menu or Exit")
+        print("*" * 29)
+        print("Welcome to ClientManager v1.0")
+        print("*" * 29)
 
-        choice = input("Enter your choice (1-5): ")
+        prompt = """
+    Select the number of your choice to display screen:   
+        [1] - Print my clients
+        [2] - Add a client
+        [3] - Edit a client
+        [4] - Delete a client
+        [5] - Exit
+    """
+        choice = input(prompt)
 
         if choice == '1':
             view_clients(clients)
